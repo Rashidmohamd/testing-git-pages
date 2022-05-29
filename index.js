6a180c8dc90a5c41019763b7a66246780fe302f8
Rashid Mohamed <rashid7252000@gmail.com>
2022-05-29 11:18:55 -0700

    added 13000 media screen part

--- index.js
+++ index.js
@@ -26,3 +26,53 @@ function show() {
    
 }
 
+
+
+//self test
+
+const attept = [
+    { name: "rashid ", age: 22, },
+    { name: "rashid ", age: 22, }
+    
+];
+
+
+function createattept(post){
+    attept.push(post)
+}
+
+createattept({name:"omar",age:55})
+
+console.log(attept);
+
+// const Rashid = {
+//     fname: "rashid",
+//     lname: "mohamed",
+//     age: 22,
+//     fullname: function () {
+//     console.log(`${this.fname} ${this.lname}`);
+// }
+
+// }
+
+
+// function Rashid(fname, lname, age, Phone) {
+//     this.fname = fname;
+//     this.lname = lname;
+//     this.age = age;
+//     this.Phone = Phone;
+// }
+
+// Rashid.prototype.fullname = function () {
+//     console.log(`${this.fname} ${this.lname}`);
+// }
+
+// let person = new Rashid("Rashid ", "mohamed ", 22, "+201200267507");
+
+// person.fullname();
+
+// console.log(person.Phone)
+
+
+
+
